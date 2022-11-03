0e588dd633b8cf2c2b9305ea657c70d4afac939e
Javier González Giménez <114144556+gonzgimnzjavier@users.noreply.github.com>
2022-11-03 11:29:33 +0100

    Delete ejem10.php

--- ejem10.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $nombre = 'Javier';
-    $nombreEnMayusculas = strtoupper($nombre);
-    echo $nombreEnMayusculas;
-    echo "<br/>";
-    echo "La cadena tiene una longitud de:".strlen($nombre);
-
-?>
