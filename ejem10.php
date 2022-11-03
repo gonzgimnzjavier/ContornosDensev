7e5e4c7d8bc8de3dab7c51519e1a6d77353d5f63
Javier González Giménez <114144556+gonzgimnzjavier@users.noreply.github.com>
2022-11-03 11:28:56 +0100

    Subo nuevos ejemplos de php

---
+++ ejem10.php
@@ -0,0 +1,8 @@
+<?php
+    $nombre = 'Javier';
+    $nombreEnMayusculas = strtoupper($nombre);
+    echo $nombreEnMayusculas;
+    echo "<br/>";
+    echo "La cadena tiene una longitud de:".strlen($nombre);
+
+?>
